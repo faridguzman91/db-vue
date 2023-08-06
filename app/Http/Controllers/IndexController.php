b390f9ebddf0c550796f4102e30c90e78a23b199
Farid Guzman <ghostrider_x101@msn.com>
2023-08-06 23:04:45 +0200

    modified routes, index cintroller

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -6,34 +6,42 @@ use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
-    //
-    public function index()
-    {
-        return inertia('Index/Index');
-    }
+  //
+  public function index()
+  {
+    return inertia('Index/Index');
+  }
 
-    public function show()
-    {
-        return inertia('Index/Show');
-    }
+  public function show()
+  {
+    return inertia('Index/Show');
+  }
 
-    public function bio()
-    {
-        return "Bio/Index";
-    }
+  public function bio()
+  {
+    return "Bio/Index";
+  }
 
-    public function exhibitions()
-    {
-        return "Exhibitions/Index";
-    }
+  public function exhibitions()
+  {
+    return "Exhibitions/Index";
+  }
 
-    public function gallery()
-    {
-        return "Gallery/Index";
-    }
+  public function gallery()
+  {
+    return "Gallery/Index";
+  }
 
-    public function blog()
-    {
-        return "Blog/Index";
-    }
+  public function blog()
+  {
+    return "Blog/Index";
+  }
+  public function cv()
+  {
+    return "CV/Index";
+  }
+  public function contact()
+  {
+    return "Contact/Index";
+  }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\IndexController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -16,23 +17,31 @@ use Inertia\Inertia;
 |
 */
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+// Route::get('/', function () {
+//     return Inertia::render('Welcome', [
+//         'canLogin' => Route::has('login'),
+//         'canRegister' => Route::has('register'),
+//         'laravelVersion' => Application::VERSION,
+//         'phpVersion' => PHP_VERSION,
+//     ]);
+// });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return Inertia::render('Dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
 
-require __DIR__.'/auth.php';
+// require __DIR__.'/auth.php';
+
+Route::get('/', [IndexController:: class, 'index']);
+Route::get('/show', [IndexController:: class, 'show']);
+Route::get('/cv', [IndexController:: class, 'cv']);
+Route::get('/exhibitions', [IndexController:: class, 'exhibitions']);
+Route::get('/gallery', [IndexController:: class, 'gallery']);
+Route::get('/blog', [IndexController:: class, 'blog']);
+Route::get('/contact', [IndexController:: class, 'contact']);
